ebb02f1408b602c18524733d0e95148cc8fa61b5
Adrien Fuss <a.fuss@stuart.com>
2022-04-04 14:14:56 +0200

    chapter 1 -  moving _amount_for function to PerformanceCalculator

--- chapter_1/create_statement_data.py
+++ chapter_1/create_statement_data.py
@@ -8,6 +8,21 @@ class PerformanceCalculator:
         self.performance = a_performance
         self.play = a_play
 
+    def amount(self):
+        result = 0
+        if self.play ["type"] == "tragedy":
+            result = 40000
+            if self.performance["audience"] > 30:
+                result += 1000 * (self.performance["audience"] - 30)
+        elif self.play ["type"] == "comedy":
+            result = 30000
+            if self.performance['audience'] > 20:
+                result += 10000 + 500 * (self.performance["audience"] - 20)
+            result += 300 * self.performance["audience"]
+        else:
+            raise Exception(f'Unknown type: {self.play["type"]}')
+        return result
+
 
 def create_statement_data(invoice: Dict[str, Any], plays: Dict[str, Any]) -> str:
     def play_for(perf):
