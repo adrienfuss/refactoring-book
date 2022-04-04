81d918d48de2c590cec389bc052ade7f921445d0
Adrien Fuss <a.fuss@stuart.com>
2022-04-04 13:49:43 +0200

    chapter 1 -  add render_html

--- chapter_1/statement.py
+++ chapter_1/statement.py
@@ -3,15 +3,31 @@ from typing import Dict, Any
 
 from chapter_1.create_statement_data import create_statement_data
 
+def usd(value: float) -> str:
+    return f'${value / 100:,.2f}'
+
+def html_statement(invoice: Dict[str, Any], plays: Dict[str, Any]) -> str:
+    return render_html(create_statement_data(invoice, plays))
+
 
 def statement(invoice: Dict[str, Any], plays: Dict[str, Any]) -> str:
     return render_plain_text(create_statement_data(invoice, plays))
 
 
-def render_plain_text(data):
-    def usd(value: float) -> str:
-        return f'${value / 100:,.2f}'
+def render_html(data):
+    result = f"<h1>Statement for {data['customer']}</h1>\n"
+    result += "<table>\n"
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>"
+    for a_performance in data['performances']:
+        result += f'  <tr><td>{a_performance["play"]["name"]}</td><td>{a_performance["audience"]}</td>\n'
+        result += f"<td>{usd(a_performance['amount'])}</td></tr>\n"
+    result += "</table>\n"
+    result += f'<p>Amount owed is <em>{usd(data["total_amount"])}</em></p>\n'
+    result += f'<p>You earned <em>{data["total_volume_credits"]}</em> credits</p>\n'
+    return result
 
+
+def render_plain_text(data):
     result = f"Statement for {data['customer']}\n"
     for a_performance in data['performances']:
         result += f'    {a_performance["play"]["name"]}: {usd(a_performance["amount"])} ({a_performance["audience"]} seats)\n'
